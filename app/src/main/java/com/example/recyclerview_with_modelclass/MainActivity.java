dcc203ae031964be30c51a19db02d9d23dc0340b
moinakash <moinakashll@gmail.com>
2021-02-02 10:30:28 +0600

    searchview added

--- app/src/main/java/com/example/recyclerview_with_modelclass/MainActivity.java
+++ app/src/main/java/com/example/recyclerview_with_modelclass/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +26,86 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager( new LinearLayoutManager(this));
 
+        searchView = (SearchView) findViewById(R.id.searchmain);
+
         Memories_list = new ArrayList<>();
 
-        for (int i = 0; i<=10; i++){
+     /*   for (int i = 0; i<=10; i++){
 
             Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Mashrafee Bin Murtaza","Cox's Bazar Tour",
                     ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
                     R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
                     "12 Likes","5 Comments","2 Shares"));
-        }
+        }*/
+
+
+        Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Mashrafee Bin Murtaza","Cox's Bazar Tour",
+                ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
+                R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
+                "12 Likes","5 Comments","2 Shares"));
+
+        Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Shakib Al Hasan","Cox's Bazar Tour",
+                ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
+                R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
+                "12 Likes","5 Comments","2 Shares"));
+
+        Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Mushfiqur Rahman","Cox's Bazar Tour",
+                ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
+                R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
+                "12 Likes","5 Comments","2 Shares"));
+
+        Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Mahmudullah Riyad","Cox's Bazar Tour",
+                ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
+                R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
+                "12 Likes","5 Comments","2 Shares"));
+
+        Memories_list.add(new Memories_modelCLass(R.drawable.ic_launcher_background,"Sabbir Rahman","Cox's Bazar Tour",
+                ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been they text ever since the 1500s ..... See More",
+                R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
+                "12 Likes","5 Comments","2 Shares"));
 
         adapter = new MemoriesAdapter(Memories_list,this);
 
         recyclerView.setAdapter(adapter);
+
+
+        searchView.setOnQueryTextListener( new SearchView.OnQueryTextListener() {
+
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayList<Memories_modelCLass> templist = new ArrayList<>(  );
+
+                for(Memories_modelCLass temp : Memories_list)
+                {
+                    if (temp.getProfile_Name().toLowerCase().contains( newText.toLowerCase() ))
+                    { templist.add( temp ); }
+                }
+
+
+                   /* CustomListAdapter adapter = new CustomListAdapter(
+                            getApplicationContext(), R.layout.custom_list_layout, templist
+                    );
+                    lv.setAdapter(adapter);*/
+
+                 adapter = new MemoriesAdapter(
+                         templist,MainActivity.this
+                );
+                recyclerView.setAdapter(adapter);
+
+
+                return true;
+            }
+
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+        } );
+
+
+
     }
 }
